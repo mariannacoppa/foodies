87cb97b5f5b7e6adf7a29c518f3fb71ce46b7469
Marianna Coppa <densimetra@gmail.com>
2025-04-10 11:59:32 +0200

    Done lesson 117: added image preview to the picker

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,15 +1,44 @@
 'use client';
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import classes from './image-picker.module.css';
+import Image from "next/image";
 export default function imagePicker({label, name}) {
+    const [pickedImage, setPickedImage] = useState();
     const imageInput = useRef();
     function handlePickClick() {
         imageInput.current.click();
     }
+
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+
+        if (!file) {
+            return;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        }
+
+        fileReader.readAsDataURL(file);
+    }
+
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && (
+                        <Image
+                            src={pickedImage}
+                            alt={"The image selected by the user."}
+                            fill
+                        />
+                    )}
+                </div>
                 <input
                     className={classes.input}
                     type={"file"}
@@ -17,6 +46,7 @@ export default function imagePicker({label, name}) {
                     accept={"image/jpeg, image/png"}
                     name={name}
                     ref={imageInput}
+                    onChange={handleImageChange}
                 />
                 <button className={classes.button} type={"button"} onClick={handlePickClick}>
                     Pick an image
