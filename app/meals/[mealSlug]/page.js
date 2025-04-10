bdaf6e14fe1adf0f82dca80dafb97b2dfb992205
Marianna Coppa <densimetra@gmail.com>
2025-04-10 09:50:59 +0200

    Done lesson 114: loading & rendering meal details via dynamic routes & route parameters

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,28 @@
-export default function MealDetailsPage({params}) {
+import classes from 'next/image';
+import Image from "next/image";
+import {getMeal} from "@/lib/meals";
+export default function MealDetailsPage({params }) {
+    const meal = getMeal(params.mealSlug);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br>');
     return (
-        <div style={{ textAlign: 'center' }}>
-            <h1 style={{ color: 'white' }}>Meal Details</h1>
-            <p className={"link"}>{params.slug}</p>
-        </div>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <mian>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}></p>
+            </mian>
+        </>
     );
 }
---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+//'use client';
+export default function NotFound() {
+    return (
+        <main className={"not-found"}>
+            <p>Not Found</p>
+            <p>Unfortunately, we could not find the requested page or resource.</p>
+        </main>
+    );
+}
